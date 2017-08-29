3341f7542f41d3967f8eabf99f2676c5e1ee0e7f
WLT10003ED0\990598 <veredz@elta.co.il>
2017-08-29 06:06:12 +0300

    Open file dialog

--- tester/src/app/app.component.ts
+++ tester/src/app/app.component.ts
@@ -14,22 +14,30 @@ import {HttpClient} from '@angular/common/http';
 export class AppComponent {
 
   app: AppClass;
+  selectedOption: string;
 
   constructor(private http: HttpClient) {
 
     const file = './person.json';
     this.app = new AppClass(http);
-
-    this.app.openFile(file);
-
-    /*if (router!=null)
-      console.log (router);*/
-    console.log ('constructor');
-
   };
 
-  openFile() {
-    console.log ('openFile');
-    /*this.app.openFile('person.json');*/
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if(fileList.length > 0) {
+      let file: File = fileList[0];
+      this.upload ('post','',file);
+    }
+  }
+
+  upload(method: string, payload: any = {}, file: any): any {
+    let params = new FormData();
+    params.append("file", file);
+    for (let key in payload) {
+      params.append(key, payload[key]);
+    }
+    let headers = new Headers();
+    headers.append('Content-Type', 'multipart/form-data');
+    return this.http.post(`${'/api'}/${method}`, params, headers);
   }
 }
