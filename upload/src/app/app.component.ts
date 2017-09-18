cfc4f0305f38d5607f365283fed28b23847ee03b
WLT10003ED0\990598 <veredz@elta.co.il>
2017-09-19 00:36:13 +0300

    wrong file was uploaded

--- upload/src/app/app.component.ts
+++ upload/src/app/app.component.ts
@@ -14,19 +14,19 @@ export class AppComponent {
   constructor(private http: Http/*private http: HttpClient*/)
   {}
 
-  addItem(List) {
-    console.log (List);
-    let fileList: FileList = List;
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+
     let file: File = fileList[0];
-    let body:FormData = new FormData();
-    body.append('uploadFile', file, file.name);
+    let body: FormData = new FormData();
     let headers = new Headers();
-    /** No need to include Content-Type in Angular 4 */
+    // No need to include Content-Type in Angular 4 
     headers.append('Content-Type', 'multipart/form-data');
+    console.log ('name='+file.name+ ' size='+file.size);
     headers.append('Accept', 'application/json');
     let options = new RequestOptions({ headers: headers });
     body.append('uploadFile', file, file.name);
-    
-    this.http.post(`${'http://192.168.14.66'}`, body, options).subscribe (data => {console.log ('response',data);});
+
+    this.http.post(`${'http://localhost:80'}`, body, options).subscribe (data => {console.log ('response',data);});
   }
 }
