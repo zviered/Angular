98efed80ebf9daa7b3b09c8d68f1164f7cafc7f3
WLT10003ED0\990598 <veredz@elta.co.il>
2017-09-21 08:07:22 +0300

    HttpClient -> Http

--- request/src/app/app.component.ts
+++ request/src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Http} from '@angular/http';
 import {IsAliveRequest} from '../icd/PcToTgtIcd';
 import {IsAliveReply} from '../icd/PcToTgtIcd';
 import {WriteWaveformRequest} from '../icd/PcToTgtIcd';
+import {ReadInfoRequest} from '../icd/PcToTgtIcd';
 //import {HTTP} from '../services/http/http-service.service';
 
 @Component({
@@ -14,26 +15,18 @@ import {WriteWaveformRequest} from '../icd/PcToTgtIcd';
 
 export class AppComponent {
   title = 'app works!';
-  constructor(private http: HttpClient)
+  constructor(private http: Http)
   {
     var request = IsAliveRequest;
     var response = IsAliveReply;
     
-    var ReadInfoRequest = {
-      "id":2,
-      "device":0
-  }
     console.log ('constructor');
 
-    console.log (request);
+    console.log (ReadInfoRequest);
 
-    let body: any;
-    body = ReadInfoRequest;
-    this.http.post('http://localhost:80', 
-    JSON.stringify({
-      body: 'bar',
-      title: 'foo',
-      userId: 1
-    })).subscribe (data => {console.log ('response',data);});
+    var myJSON = JSON.stringify(ReadInfoRequest);
+    console.log ('r='+myJSON);
+    this.http.post('http://localhost:80',myJSON).subscribe (data => {console.log ('response',data);});
+  
   }
 }
