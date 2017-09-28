5b2f01758936703cb1ef8fc124f692d12636e8ad
zvivered <veredz72@gmail.com>
2017-09-28 05:41:58 +0300

    Add RcmTester

--- RcmTester/src/app/app.component.ts
+++ RcmTester/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  private readData : string;
+
+  constructor ()
+  {
+    this.readData = "";
+  }
+  writeWord (deviceId, offset, data)
+  {
+    console.log ("write to device["+deviceId+"] offset="+offset+" data="+data);
+  }
+
+  readWord (deviceId, offset)
+  {
+    console.log ("read from device["+deviceId+"] offset="+offset+" data=?");
+    this.readData = "0x20";
+  }
 }
